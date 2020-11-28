84ffcc0ac84b4341598f16d71c411b21bd17613a
Deepak Venkatachalam <dvenkatachalam@atlassian.com>
2020-11-28 11:17:18 -0500

    Add material ui and create a nbjc palette.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import { ThemeProvider } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+
 import logo from './logo.svg';
 import './App.css';
 
+import { palette } from './components/theme';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NBJC Webapp
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={palette}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Button color="primary" variant="contained" disableElevation>Primary</Button>
+            <Button variant="outlined">default</Button>
+            <Button color="secondary">Secondary</Button>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              NBJC Webapp
+            </a>
+          </header>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/theme/palette.js
@@ -0,0 +1,27 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#A96EB8',
+      main: '#752A87',
+      dark: '#480658'
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#E083AA',
+      main: '#AE3367',
+      dark: '#720432',
+      contrastText: '#ffcc00',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+});
+
+export default palette;
