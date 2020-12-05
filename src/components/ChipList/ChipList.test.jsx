99e6d956b73ad800cc5235ec3f9bfd7da7963050
Deepak Venkatachalam <dvenkatachalam@atlassian.com>
2020-12-05 18:31:03 -0500

    Add ChipList component to handle chip display logic.

---
+++ src/components/ChipList/ChipList.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ChipList from './ChipList';
+
+jest.mock('react-intersection-observer', () =>({
+  useInView: () => [{}, true],
+}));
+
+describe('ChipList', () => {
+  test('render', () => {
+    const chips = [{
+      name: 'Black Friendly',
+    }, {
+      name: 'Inclusive',
+    }, {
+      name: 'Black Owned',
+    }, {
+      name: 'Gender Neutral Restrooms',
+    }, {
+      name: 'Accessible',
+    }, {
+      name: 'Queer hangout space',
+    }, {
+      name: 'Trans friendly',
+    }, {
+      name: 'Queer owned',
+    }];
+    const { asFragment } = render(<ChipList chips={chips} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
