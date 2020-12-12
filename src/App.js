a079591177db70b3f641b93408247086ded06b30
Lianna Newman <liannanewman@gmail.com>
2020-12-12 15:43:59 -0500

    Created Space Details Page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
-import {
-  BrowserRouter as Router, Link, Switch, Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
 
 import theme from './theme';
 import { Home, Search } from './routes';
