12076e605282abbc6f37cd9616b88d2a34d6017e
Deepak Venkatachalam <dvenkatachalam@atlassian.com>
2020-12-12 15:48:22 -0500

    Add chips filter component.

---
+++ src/components/ChipFilters/ChipFilters.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import ChipFilters from './ChipFilters';
+
+describe('ChipFilters', () => {
+  test('render', () => {
+    const chips = [{
+      name: 'Black Friendly',
+    }, {
+      name: 'Inclusive',
+    }, {
+      name: 'Black Owned',
+    }, {
+      name: 'Gender Neutral Restrooms',
+    }, {
+      name: 'Accessible',
+    }, {
+      name: 'Queer hangout space',
+    }, {
+      name: 'Trans friendly',
+    }, {
+      name: 'Queer owned',
+    }];
+    const { asFragment } = render(<ChipFilters chips={chips} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
