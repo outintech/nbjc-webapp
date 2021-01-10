42c1c949a14ef89501f91e48f1fa7e696988bfc1
Deepak Venkatachalam <dvenkatachalam@atlassian.com>
2021-01-10 13:08:57 -0500

    Get space id from path.

--- src/routes/AddReview/AddReview.jsx
+++ src/routes/AddReview/AddReview.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const AddReview = () => (
-  <h3>Add Review page</h3>
-);
+const AddReview = () => {
+  const { spaceId } = useParams();
+  // todo: call backend to verify user does not have review for space
+  return (
+    <>
+      <h3>Review for</h3>
+      <>{spaceId}</>
+    </>
+  );
+};
 
 export default AddReview;
