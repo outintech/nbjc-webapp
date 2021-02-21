aef51726409e60ecc477956012a6f72a0dc4e544
Deepak Venkatachalam <dvenkatachalam@atlassian.com>
2021-02-21 10:35:09 -0500

    Fix double quotes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,10 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'no-plusplus': 0,
   },
-  "globals": {
-    "window": true,
-    "navigator": true,
-    "document": true,
+  globals: {
+    window: true,
+    navigator: true,
+    document: true,
   },
   overrides: [{
     files: ['*.test.js', '*.test.jsx'],
