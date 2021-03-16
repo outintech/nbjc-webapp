d3db593922924ead339420c6f535c8ba619fe926
Shayne Sebro <shaynesebro@gmail.com>
2021-03-15 22:47:20 -0400

    Typography updates

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -11,12 +11,6 @@ import {
 } from '@material-ui/core';
 
 const styles = () => ({
-  root: {
-    maxHeight: 'lg',
-    overflow: 'hidden',
-    backgroundColor: 'black',
-    display: 'flex',
-  },
   container: {
     maxHeight: 'lg',
     overflow: 'hidden',
@@ -24,13 +18,25 @@ const styles = () => ({
     flexDirection: 'column',
     justifyContent: 'center',
   },
+  title: {
+    textAlign: 'center',
+  },
+  blurb: {
+    textAlign: 'center',
+    margin: '0 auto 20px auto',
+    width: '80%',
+  },
   identityChip: {
-    marginBottom: 8,
-    marginRight: 4,
+    marginBottom: '8px',
+    marginRight: '4px',
   },
   textInput: {
     width: '100%',
     margin: '10px 0 20px 0',
+  },
+  label: {
+    paddingLeft: '10px',
+    color: 'black',
   },
 });
 
@@ -39,7 +45,7 @@ const userLabels = ['Agender', 'Aliagender', 'Ally', 'Androgyne', 'Arab', 'Aroma
 
 const ProfilePage = ({ classes }) => {
   const [profileInfo, setProfileInfo] = useState({
-    selectedLabels: ['Agender', 'Aliagender', 'Ally', 'Androgyne', 'Arab', 'Aromantic', 'Asexual'],
+    selectedLabels: ['Bisexual', 'Black', 'Gender Fluid'],
     name: 'Name',
     pronouns: 'pronouns',
     location: 'location',
@@ -77,12 +83,15 @@ const ProfilePage = ({ classes }) => {
 
   return (
     <Container className={classes.container}>
-      <Typography>
+      <Typography variant="h4" className={classes.title}>
         Users Profile
+      </Typography>
+      <Typography className={classes.blurb}>
+        View and edit your profile. After you make a change, click Save.
       </Typography>
       <form>
         <InputLabel type="inputLabel">
-          <Typography>Name</Typography>
+          <Typography className={classes.label}>Name</Typography>
         </InputLabel>
         <TextField
           onChange={handleChange}
@@ -94,9 +103,10 @@ const ProfilePage = ({ classes }) => {
           name="name"
           autoFocus
           className={classes.textInput}
+          required
         />
         <InputLabel type="inputLabel">
-          <Typography>Pronouns</Typography>
+          <Typography className={classes.label}>Pronouns</Typography>
         </InputLabel>
         <TextField
           onChange={handleChange}
@@ -107,9 +117,10 @@ const ProfilePage = ({ classes }) => {
           placeholder="User Name"
           name="pronouns"
           className={classes.textInput}
+          required
         />
         <InputLabel type="inputLabel">
-          <Typography>Location</Typography>
+          <Typography className={classes.label}>Location</Typography>
         </InputLabel>
         <TextField
           onChange={handleChange}
@@ -120,6 +131,7 @@ const ProfilePage = ({ classes }) => {
           placeholder="User Name"
           name="location"
           className={classes.textInput}
+          required
         />
         <Typography variant="h6">Tell us about yourself</Typography>
         <Box>
