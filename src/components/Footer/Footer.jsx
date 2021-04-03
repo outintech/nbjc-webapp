4bbc90c91035f694993de5acce17897642c75454
Deepak Venkatachalam <dvenkatachalam@atlassian.com>
2021-04-03 12:30:49 -0400

    Update footer styles.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 // import Button from '@material-ui/core/Button';
 
@@ -26,12 +26,16 @@ const styles = (theme) => ({
   info: {
     marginTop: 10,
     marginLeft: 20,
+    paddingRight: 20,
     [theme.breakpoints.up('xs')]: {
       marginRight: 0,
       width: '100%',
     },
     [theme.breakpoints.up('mobile')]: {
       width: 'unset',
+    },
+    '& a': {
+      fontWeight: 'bold',
     },
   },
   links: {
@@ -42,7 +46,7 @@ const styles = (theme) => ({
       marginLeft: 20,
     },
     [theme.breakpoints.up('mobile')]: {
-      marginTop: 0,
+      marginTop: 20,
       marginLeft: 'auto',
     },
   },
@@ -52,19 +56,24 @@ const styles = (theme) => ({
   },
   link: {
     float: 'right',
+    fontSize: 14,
     '& a': {
       color: 'inherit',
       marginRight: 20,
+      whiteSpace: 'pre-wrap',
+    },
+    '& a:last-of-type': {
+      marginRight: 0,
     },
   },
-  divider: {
-    height: 20,
-    marginRight: 5,
-    backgroundColor: theme.palette.primary.contrastText,
-    display: 'inline',
-  },
   copyright: {
-    whiteSpace: 'nowrap',
+    '&::before': {
+      content: '""',
+      marginRight: 10,
+      marginLeft: 10,
+      borderLeft: '1px solid white',
+      whiteSpace: 'pre-wrap',
+    },
   },
 });
 
@@ -84,7 +93,7 @@ const Footer = ({ classes }) => (
       </Typography>
     </div>
     <div className={classes.links}>
-      <Typography variant="overline" className={classes.link}>
+      <Typography className={classes.link}>
         <a size="small" href="/community-guidelines" disableRipple disableFocusRipple disableTouchRipple>
           Community Guidelines
         </a>
@@ -94,8 +103,9 @@ const Footer = ({ classes }) => (
         <a size="small" href="/privacy-policy" disableRipple disableFocusRipple disableTouchRipple>
           Privacy Policy
         </a>
-        <Divider orientation="vertical" className={classes.divider} />
-        Copyright 2021 NBJC
+        <Box component="span" className={classes.copyright}>
+          Copyright 2021 NBJC
+        </Box>
       </Typography>
     </div>
   </div>
