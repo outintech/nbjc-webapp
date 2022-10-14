0a4c7d537752823cea177ac23ec1df0c30ec88ba
Amy <86702974+ApplePieAngel@users.noreply.github.com>
2022-10-14 12:56:45 -0500

    DEV-82:  Add Autocomplete to Homepage (#68)
    Added autocomplete with States onto Homepage redesign.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { usePromiseTracker } from 'react-promise-tracker';
+import { useHistory } from 'react-router-dom';
+
+import { geolocated } from 'react-geolocated';
+
+import { withStyles } from '@material-ui/core/styles';
+
 import {
   Box,
   IconButton,
-  InputBase,
+  TextField,
   Paper,
-  Typography,
 } from '@material-ui/core';
+
 import SearchIcon from '@material-ui/icons/Search';
-import { useHistory } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+
+import States from '../../api/states';
+import useSearch from '../../routes/Search/hooks/useSearch';
+
+const filterOptions = createFilterOptions({
+  limit: 5,
+  trim: true,
+  stringify: (option) => option.name,
+});
 
 const styles = (theme) => ({
   root: {
     display: 'flex',
     alignItems: 'center',
-    // width: theme.spacing(80),
     [theme.breakpoints.up('xs')]: {
       width: theme.spacing(34),
     },
@@ -34,16 +49,39 @@ const styles = (theme) => ({
   input: {
     display: 'flex',
     flexGrow: 1,
+    alignItems: 'left',
+  },
+  icon: {
+    color: theme.palette.primary.main,
+    display: 'flex',
+  },
+  dropdown: {
+    display: 'flex',
     alignItems: 'center',
+    flexWrap: 'wrap',
+    fontWeight: 'bold',
   },
 });
 
-const SearchBar = ({ classes }) => {
+const SearchBar = ({
+  classes,
+  coords,
+  isGeolocationEnabled,
+}) => {
   const history = useHistory();
-  const title = 'Where';
   const placeholder = 'City, state, or zip code';
-  const ariaLabel = `Search ${placeholder}`;
+  const { promiseInProgress } = usePromiseTracker();
+  const { userLocation } = useSearch({ userCoords: coords, isGeolocationEnabled });
+  const geopositionLoading = promiseInProgress || (isGeolocationEnabled && coords === null);
+
   const [location, setLocation] = useState('');
+  const [autofillWithBlankInput, setAutoFillWithBlankInput] = useState([]);
+
+  useEffect(() => {
+    if (isGeolocationEnabled && !geopositionLoading && userLocation.address !== undefined) {
+      setAutoFillWithBlankInput([{ name: userLocation.address.city }]);
+    }
+  }, [geopositionLoading]);
 
   const handleTextInputChange = (event) => {
     setLocation(event.target.value);
@@ -54,27 +92,57 @@ const SearchBar = ({ classes }) => {
     event.preventDefault();
     history.push({
       pathname: '/search/results',
-      search: `?location=${location.toString()}`,
+      search: `?searchTerm=&category=&location=${location.trim()}`,
     });
+  };
+
+  const parseLocationObjectToString = (obj) => {
+    let locationObjString = `${obj.name}`;
+    if (obj.abbreviation) {
+      locationObjString += `, ${obj.abbreviation}`;
+    }
+    return locationObjString;
   };
 
   return (
     <Paper component="form" onSubmit={handleSubmit}>
       <Box className={classes.root}>
-        <Typography
-          className={classes.title}
-          color="secondary"
-          style={{ fontWeight: 'bolder' }}
-        >
-          {title}
-        </Typography>
-        <InputBase
+        <Autocomplete
           className={classes.input}
-          data-testid="searchbar-input"
-          placeholder={placeholder}
-          inputProps={{ 'aria-label': ariaLabel }}
-          value={location}
-          onChange={handleTextInputChange}
+          options={location.trim() === '' ? autofillWithBlankInput : States}
+          getOptionSelected={(option, value) => option.name === value.name}
+          getOptionLabel={(option) => parseLocationObjectToString(option)}
+          onChange={(event, newLocation) => {
+            setLocation(parseLocationObjectToString(newLocation));
+          }}
+          forcePopupIcon={false}
+          disableClearable
+          filterSelectedOptions
+          filterOptions={filterOptions}
+          renderOption={(props) => {
+            const dropdownText = parseLocationObjectToString(props);
+            return (
+              <Box
+                component="div"
+                sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+                className={classes.dropdown}
+              >
+                <LocationOnIcon className={classes.icon} />
+                {dropdownText}
+              </Box>
+            );
+          }}
+          renderInput={(params) => (
+            <TextField
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...params}
+              variant="outlined"
+              disableunderline="true"
+              InputProps={{ ...params.InputProps, style: { padding: 5 } }}
+              onChange={handleTextInputChange}
+              placeholder={placeholder}
+            />
+          )}
         />
         <Box
           display="flex"
@@ -99,4 +167,4 @@ SearchBar.propTypes = {};
 
 SearchBar.defaultProps = {};
 
-export default withStyles(styles)(SearchBar);
+export default geolocated({ positionOptions: { timeout: 5000 } })(withStyles(styles)(SearchBar));
