a2a0fbeecf671a074f5feba455b772086b0bf294
Edward Ficklin <edward.ficklin@gmail.com>
2022-10-19 16:50:55 -0400

    state for filter vals; attempt at collapsing list

--- src/components/FilterPanel/FilterPanel.jsx
+++ src/components/FilterPanel/FilterPanel.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars, no-console */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -12,6 +12,10 @@ import {
   FormControlLabel,
   Checkbox,
   InputAdornment,
+  FormControl,
+  FormLabel,
+  FormGroup,
+  Collapse,
 } from '@material-ui/core';
 
 import CloseIcon from '@material-ui/icons/Close';
@@ -22,8 +26,10 @@ const styles = {
     justifyContent: 'flex-end',
   },
   dialogBody: {
-    padding: '0 24px',
+    display: 'flex',
+    flexDirection: 'column',
     marginBottom: 15,
+    padding: '0 24px',
   },
   header: {
     display: 'flex',
@@ -38,6 +44,13 @@ const styles = {
   },
 };
 
+const priceFilterDefault = {
+  price_1: false,
+  price_2: false,
+  price_3: false,
+  price_4: false,
+};
+
 const FilterPanel = ({
   open,
   onClose,
@@ -47,15 +60,37 @@ const FilterPanel = ({
   /* search */
   /* updateSearch */
 }) => {
-  const checkedFilters = 2;
+  const [nameFilterVal, setNameFilterVal] = useState('');
+  const [priceFilterVal, setPriceFilterVal] = useState(priceFilterDefault);
+  const [indicatorVals, setIndicatorVals] = useState({});
+  const [filterCount, setFilterCount] = useState(0);
+  const [collapsed, setCollapsed] = useState(true);
+
+  useEffect(() => {
+    let active = 0;
+    if (nameFilterVal) {
+      active++;
+    }
+    active += Object.values(priceFilterVal).filter((v) => v === true).length;
+    active += Object.values(indicatorVals).length;
+    setFilterCount(active);
+  }, [nameFilterVal, priceFilterVal, indicatorVals]);
+
+  const clearFilters = () => {
+    setNameFilterVal('');
+    setPriceFilterVal(priceFilterDefault);
+    setIndicatorVals({});
+  };
+
   const header = (
     <div className={classes.header}>
       <h2>
         Filter
-        { ` (${checkedFilters})` }
+        {filterCount > 0 && ` (${filterCount})`}
       </h2>
       <Button
         className={classes.clearButton}
+        onClick={() => clearFilters()}
       >
         Clear All
       </Button>
@@ -64,6 +99,7 @@ const FilterPanel = ({
 
   const nameFilter = (
     <TextField
+      id="nameFilter"
       type="text"
       variant="outlined"
       label="Search by name within results"
@@ -73,48 +109,81 @@ const FilterPanel = ({
           <InputAdornment position="end">
             <IconButton
               aria-label="search"
-              onClick={() => console.log('call updateSearch with input value')}
+              onClick={() => console.log('do the search!')}
             >
               <SearchIcon />
             </IconButton>
           </InputAdornment>
         ),
       }}
+      value={nameFilterVal}
+      onChange={(e) => setNameFilterVal(e.target.value)}
     />
   );
 
   const priceFilter = (
-    <div className="group">
-      <div className="groupLabel">
-        Price
-      </div>
-      <div className={classes.controls}>
+    <FormControl component="fieldset" className="">
+      <FormLabel component="legend" className="">Price</FormLabel>
+      <FormGroup>
         { [1, 2, 3, 4].map((i) => (
           <FormControlLabel
             key={`price_${i}`}
-            control={<Checkbox name={`price_${i}`} onClick={() => console.log('price checkbox!')} />}
+            control={(
+              <Checkbox
+                name={`price_${i}`}
+                checked={priceFilterVal[`price_${i}`]}
+                onClick={(e) => setPriceFilterVal(
+                  { ...priceFilterVal, [e.target.name]: e.target.checked },
+                )}
+                inputProps={{ 'aria-label': `Price, level ${i}` }}
+              />
+            )}
             label={`${'$'.repeat(i)}`}
           />
         )) }
-      </div>
-    </div>
+      </FormGroup>
+    </FormControl>
   );
 
   const indicatorFilters = (
-    <div className="group">
-      <div className="groupLabel">
-        Indicators
-      </div>
-      <div className={classes.controls}>
-        { indicators.map((i) => (
-          <FormControlLabel
-            key={`indicator_${i.value}`}
-            control={<Checkbox name={`indicator_${i.name}`} onClick={() => console.log('indicator!')} />}
-            label={i.name}
-          />
-        )) }
-      </div>
-    </div>
+    <FormControl component="fieldset" className="">
+      <FormLabel component="legend" className="">Indicators</FormLabel>
+      <Collapse in={!collapsed} collapsedSize="269px">
+        <FormGroup>
+          { indicators.map((i) => (
+            <FormControlLabel
+              key={`indicator_${i.value}`}
+              control={(
+                <Checkbox
+                  name={i.name}
+                  value={i.value}
+                  checked={Object.hasOwn(indicatorVals, i.name)}
+                  onClick={(e) => {
+                    if (e.target.checked) {
+                      setIndicatorVals({ ...indicatorVals, [e.target.name]: e.target.value });
+                    } else {
+                      setIndicatorVals(
+                        {
+                          ...Object.fromEntries(
+                            Object.entries(indicatorVals).filter(
+                              ([key, val]) => key !== e.target.name,
+                            ),
+                          ),
+                        },
+                      );
+                    }
+                  }}
+                />
+              )}
+              label={i.name}
+            />
+          )) }
+        </FormGroup>
+      </Collapse>
+      <Button onClick={() => setCollapsed(!collapsed)}>
+        { `Show ${collapsed ? 'More' : 'Less'}` }
+      </Button>
+    </FormControl>
   );
 
   if (type === 'desktop') {
