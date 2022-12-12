696e13be5d9305a21e29a5e3d8bd9f60f66e504c
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-12-12 17:41:19 -0500

    Increased the margin for the CTA border

--- src/components/BusinessCard/BusinessCard.jsx
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -10,6 +10,7 @@ import {
 
 import StarIcon from '@material-ui/icons/Star';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import RateReviewIcon from '@material-ui/icons/RateReview';
 
 import { withStyles } from '@material-ui/core/styles';
 
@@ -134,7 +135,7 @@ const styles = (theme) => ({
     borderTop: '1px solid #E5E5E5',
     textAlign: 'center',
     justifyContent: 'space-between',
-    paddingTop: '5px',
+    paddingTop: '12px',
   },
   tagContainer: {
     display: 'flex',
@@ -148,6 +149,9 @@ const styles = (theme) => ({
   },
   ratingContainer: {
     display: 'flex',
+  },
+  ratingScore: {
+    color: '#633AA3',
   },
 });
 
@@ -193,13 +197,13 @@ const BusinessCard = ({
             </Box>
             <Box className={classes.CTAcontainer}>
               <Box className={classes.ratingContainer}>
-                <StarIcon color="secondary" fontSize="small" />
+                <RateReviewIcon color="secondary" fontSize="small" style={{ marginRight: 2 }} />
                 <span>Add Review</span>
               </Box>
               <Box className={classes.ratingContainer}>
-                <span>Rating</span>
+                <span style={{ marginRight: 1 }}>Rating</span>
                 <StarIcon color="secondary" fontSize="small" />
-                {averageRating}
+                <span className={classes.ratingScore}>{averageRating}</span>
               </Box>
               <Box className={classes.ratingContainer}>
                 {phoneNumber}
