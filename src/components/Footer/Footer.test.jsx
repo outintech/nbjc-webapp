1138a2a48add24981f03fc41edb1eaa3e4ed1b9d
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-12-24 16:00:11 -0500

    Added history testing for internal links and tested all the links in the footer

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 
 import Footer from './Footer';
 
-describe('Footer Component', () => {
+describe('Do the correct links exist in the Footer Component', () => {
   let container;
 
   beforeEach(() => {
@@ -38,5 +40,66 @@ describe('Footer Component', () => {
     expect(PolicyLink).toHaveAttribute('href', 'https://nbjc.org/privacy-policy/');
     expect(PolicyLink).toHaveAttribute('target', '_blank');
     expect(PolicyLink).toHaveAttribute('rel', 'noreferrer');
+  });
+  it('should check if the copyright exista and has the right year', () => {
+    const CurrentYear = new Date().getFullYear();
+    expect(screen.getByText(`Copyright ${CurrentYear} NBJC`)).toBeInTheDocument();
+  });
+  it('should check if the Donate Link element exists as a label', () => {
+    const DonateLink = screen.getByText('Donate');
+    expect(DonateLink).toBeInTheDocument();
+  });
+  it('should check for an existing Community Guidelines link', () => {
+    const CommunityGuidelinesLink = screen.getByText('Community Guidelines', { selector: 'a' });
+    expect(CommunityGuidelinesLink).toBeInTheDocument();
+  });
+});
+
+describe('Do the internal links redirect you to the right path', () => {
+  it('should check for a CommunityGuidelinessLink redirects to the correct page', async () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Footer />
+      </Router>,
+    );
+
+    const CommunityButton = screen.getByText('Community Guidelines', { selector: 'a' });
+    expect(CommunityButton).toBeInTheDocument();
+
+    userEvent.click(CommunityButton);
+    expect(history.length).toBe(2);
+    expect(history.location.pathname).toBe('/community-guidelines');
+  });
+
+  it('should check for a Terms of Service link that redirects you to that page', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Footer />
+      </Router>,
+    );
+
+    const TermsOfServiceButton = screen.getByText('Terms of Service', { selector: 'a' });
+    expect(TermsOfServiceButton).toBeInTheDocument();
+
+    userEvent.click(TermsOfServiceButton);
+    expect(history.length).toBe(2);
+    expect(history.location.pathname).toBe('/terms-of-service');
+  });
+  it('should check for a Infringement Policies link that redirects you to that page', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Footer />
+      </Router>,
+    );
+
+    const InfringementButton = screen.getByText('Infringement Policies', { selector: 'a' });
+    expect(InfringementButton).toBeInTheDocument();
+
+    userEvent.click(InfringementButton);
+    expect(history.length).toBe(2);
+    expect(history.location.pathname).toBe('/infringement-policies');
   });
 });
