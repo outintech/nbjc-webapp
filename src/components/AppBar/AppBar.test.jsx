3ea5885536c0d19e988dea612c57e363f7c42a4e
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-12-24 23:34:55 -0500

    Added initial testing setup, renders and empty tests

--- src/components/AppBar/AppBar.test.jsx
+++ src/components/AppBar/AppBar.test.jsx
@@ -1,62 +1,63 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-
-import HomeIcon from '@material-ui/icons/Home';
-import SearchIcon from '@material-ui/icons/Search';
+import { render } from '@testing-library/react';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 import AppBar from './AppBar';
-
+import { UserContext } from '../../context/UserContext';
 
 describe('AppBar', () => {
-  const routes = [{
-    label: 'Home',
-    path: '/',
-    key: 'home',
-    icon: HomeIcon,
-  }, {
-    label: 'Search for a space',
-    path: '/search',
-    key: 'search',
-    icon: SearchIcon,
-  }];
-  
-  test('renders with default state', () => {
-    const { asFragment } = render(
-      <AppBar routes={routes} />,
-      );
-      expect(asFragment()).toMatchSnapshot();
-    });
-    
-    test('renders the drawer', () => {
-      render(
-        <AppBar selected="search" routes={routes} />,
-        );
-        const pageTitle = screen.getByTestId('appbar-title');
-        expect(pageTitle.textContent).toBe('Search for a space');
-        
-        const menu = screen.getByTestId('appbar-menu');
-        fireEvent.click(menu);
-        
-        const drawer = screen.getByTestId('appbar-drawer');
-        expect(drawer).not.toBe(undefined);
-      });
-    });
+  it('should check that the lavender book logo exists and is visible', () => {
+    const dummyValue = {
+      name: 'abcd',
+      userProfile: 'abcd',
+    };
+    render(
+      <>
+        <UserContext.Provider value={dummyValue}>
+          <BrowserRouter>
+            <Switch>
+              <AppBar />
+            </Switch>
+          </BrowserRouter>
+        </UserContext.Provider>
+      </>,
 
-    const mockHistoryPush = jest.fn();
-    
-    jest.mock('react-router-dom', () => ({
-      ...jest.requireActual('react-router-dom'),
-      useHistory: () => ({
-        push: mockHistoryPush,
-      }),
-      useLocation: jest.fn().mockReturnValue({
-        path: '/search',
-        search: '',
-        hash: '',
-        state: undefined,
-        key: '',
-      }),
-    }));
-    
-    
+    );
+  });
+  it('should check if a user is not logged in that the Log In button displays', () => {
 
+  });
+  it('should check that Add a Space link exists in the AppBar', () => {
+
+  });
+  it('should remove the Add a Space text when the dimensions get smaller enough', () => {
+
+  });
+  it('should check that the navBar is white', () => {
+
+  });
+  it('should check if the user is logged in it displays the users account name instead of log in', () => {
+
+  });
+  it('should check if the users account name is too large it be concanated and displayed instead', () => {
+
+  });
+  it('should check if theres a dropdown menu when the user is logged in', () => {
+
+  });
+  it('should check that there is not a dropdown menu when the user is logged in', () => {
+
+  });
+  it('if the user is logged in the dropdown menu should allow the user to click the dropdown menu and log out', () => {
+
+  });
+  it('if the user is logged in and clicks the dropdown button if the user clicks out of it the dropdown should disappaer', () => {
+
+  });
+  it('if the user is logged in, there should be an option in the dropdown allowing the user to check their profile', () => {
+
+  });
+  it('if the user clicks on the check profile, they should be redirected to the right page', () => {
+
+  });
+});
