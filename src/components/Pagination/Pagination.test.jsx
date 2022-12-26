31b73d53c313b7eae1f81793284bf9e5488e28ea
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-12-26 00:46:34 -0500

    Adding initial test scaffolding

--- src/components/Pagination/Pagination.test.jsx
+++ src/components/Pagination/Pagination.test.jsx
@@ -7,3 +7,21 @@ test.skip('Pagination', () => {
   const { asFragment } = render(<Pagination totalCount={50} page={2} perPage={10} />);
   expect(asFragment()).toMatchSnapshot();
 });
+
+describe('Pagination testing', () => {
+  it('should get the correct pageLink given a page and perpage query', () => {
+
+  });
+  it('should render a faded next button with no link if theres no next page', () => {
+
+  });
+  it('should go to the next page when clicking on the next button if it exists', () => {
+
+  });
+  it('should render a faded back button with no link if theres no previous page', () => {
+
+  });
+  it('should go to the previous page when clicking on the back button if it exists', () => {
+
+  });
+});
