f655263a8c9e9903aff0cbc94a64028f94b32d06
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-12-26 01:34:58 -0500

    Added scaffolding for tests

---
+++ src/routes/Search/Search.test.jsx
@@ -0,0 +1,38 @@
+describe('Search Page', () => {
+  it('should hide the per page menu in the top row for smaller dimensions', () => {
+
+  });
+  it('should show the per page menu at the top for bigger resolutions', () => {
+
+  });
+  it('should show the per page menu at the bottom for smaller resolutions', () => {
+
+  });
+  it('should render menus as a bar for smallr resolutions', () => {
+
+  });
+  it('should display the correct number of results', () => {
+
+  });
+  it('should show a filter button for smaller resolutions', () => {
+
+  });
+  it('should hide the filter button for larger resolutions', () => {
+
+  });
+  it('should open the filter panel if the filter button is clicked', () => {
+
+  });
+  it('if there are no results it should properly render an 0 results seen', () => {
+
+  });
+  it('should properly render the correct number of business cards given a per page amount', () => {
+
+  });
+  it('should render a dropdown menu when clicking the sort by menus', () => {
+
+  });
+  it('should update the current placeholder value when clicking on the dropdown menu values', () => {
+
+  });
+});
