07414d8019dc9b8a4f764229f39104d2cccd8c4c
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-12-28 19:51:05 -0500

    Added three more functions for test cases.

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/';
 
 import Footer from './Footer';
 
@@ -13,17 +14,38 @@ const renderFooter = () => (
 );
 
 const itHasCorrectLabel = (linkName) => {
-  it('Renders as a link', () => {
+  it('It has the correct label', () => {
     renderFooter();
     expect(screen.getByText(linkName)).toBeInTheDocument();
   });
 };
 
+const itRendersAsALink = (linkName) => {
+  it('It renders as a link', () => {
+    renderFooter();
+    expect(screen.getByText(linkName)).toHaveAttribute('href');
+  });
+};
+
+const itHasCorrectHref = (linkName, hrefString) => {
+  it('It has the correct href', () => {
+    renderFooter();
+    expect(screen.getByText(linkName)).toHaveAttribute('href', hrefString);
+  });
+};
+
+const itIsExternalLink = (linkName) => {
+  it('It is an external link', () => {
+    renderFooter();
+    expect(screen.getByText(linkName)).toHaveAttribute('target', '_blank');
+  });
+};
+
 describe('Privacy Policy Link', () => {
   const policyLabel = 'Privacy Policy';
-  // itRendersAsALink(policyLabel);
-  // itIsExternalLink(policyLabel);
-  // itHasCorrectHref(policyLabel, 'https://nbjc.org/privacy-policy/');
+  itRendersAsALink(policyLabel);
+  itIsExternalLink(policyLabel);
+  itHasCorrectHref(policyLabel, 'https://nbjc.org/privacy-policy/');
   itHasCorrectLabel(policyLabel);
   // itRedirectsOnClick(policyLabel);
 });
