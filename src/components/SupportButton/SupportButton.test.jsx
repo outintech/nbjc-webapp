fe87e3155d66f72d92cb662fd247a2022f2487b1
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2023-01-01 21:53:59 -0500

    Added one test for Support Button

--- src/components/SupportButton/SupportButton.test.jsx
+++ src/components/SupportButton/SupportButton.test.jsx
@@ -1,10 +1,12 @@
-/*
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 
 import SupportButton from './SupportButton';
 
-test('SupportButton', () => {
-
+describe('SupportButton', () => {
+  it('It should render a correct label', () => {
+    render(<SupportButton />);
+    const button = screen.getByText('Report a Problem');
+    expect(button).toBeInTheDocument();
+  });
 });
-*/
