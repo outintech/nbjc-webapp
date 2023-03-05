b2cdb8aa3c04be869312aed73e7686da9b068d28
Amy <86702974+ApplePieAngel@users.noreply.github.com>
2023-03-05 16:08:18 -0600

    DEV-97.2: Pagination, Card, Refactoring and Improvements (#81)
    * Fixed a bug where Rating wasn't inheriting the right color
    
    * Updated pagination with larger icons for the next, back buttons
    
    * Fixed a bug with Pagination where the buttons now render as gray
    
    * Fixed a bug with ToGoPageX, deleted unnecessary override
    
    * Realigned the buttons to be in the center of the purple box
    
    * Fixed bug with text overflowing the card component
    
    * Refactored out some of the CTA logic
    
    * Refactored some rendundant classes, address now links in a new tab
    
    * Added a lot of refactoring for redundant css
    
    * Added min height for CTA bar
    
    * Added additional margins to the Chips
    
    * Cleanly render images without the use of booleans
    
    * Added 16px margin around the card
    
    * DEV-97.3: Search Page Menus and Mobile Menus (#82)
    
    * Added components for search options, aligned text and icon
    
    * Renamed class names for readability, created parent component for Sort
    
    * Sort by bars now extend the length of the page on smaller dimensions
    
    * Refactoring component tree into smaller components
    
    * Header containing num of search results shrinks in smaller dimensions
    
    * Reduced the margin between cards and pagination
    
    * Sort bars now collapse at a smaller width
    
    * Added extra font weight to the rating CTA
    
    * Refactored some redudancy with props drilling
    
    * Added dropdown for share button
    
    * Fixed a bug regarding Google Maps and locations
    
    * Replaced hrefs with react router links prevent re-rendering
    
    * Added clear all button and refactored redundant component
    
    * Changed Clear All button to be the same size as filter buutton
    
    * Deleted unused css, added coloring and borders to the Clear All button
    
    * Refactored props composition with Menus
    
    * Fixed cards not extending the max length, changed some menu text
    
    * Added back the new menu labels for all the menus
    
    * Refactored components into one object to make it easier to edit
    
    * Phone number buttons no longer appear if there's no input
    
    * Align the filter and sort menus for the desktop view
    
    * Users can no longer go to the current page with pagination.
    
    * Menu now renders under the button rather than off to the side
    
    * Changed the Buttons to ListItems, it now changes and saves the current value.
    
    * Scrapped list items and implemented changing values with buttons
    
    * The selected menu item label has a bolding effect
    
    * Changed dropdown menu to gray and selected option is grayer with box shadow
    
    * Added additional line height to mobile menus
    
    * Adding initial test scaffolding
    
    * Adding initial test scaffolding
    
    * Added scaffolding for tests
    
    * Added scaffolding to business card tests.
    
    * Fixed no key in list error, fixed typo in className and fixed unused MUI element
    
    * Refactoring all class names to be camelCase
    
    * Added box shadow to apply box and removed bottom margin for mobile view
    
    * Removed unnecessary prop causing errors.
    
    * Removed empty tests, abstracted out pagination function
    
    * Added test cases for next button.
    
    * Added tests for check valid page number
    
    * Testing range of results
    
    * Added test cases for reviewCTA, addressRow, titleBar, ShareMenu, OrdinalNumber, formatPhone#, convertAdd..
    
    * Added tests for business cards regarding CTA components
    
    * Added tests for SortByMenu
    
    * DEV-97.4: Add Report Problem Button in Results Page (#84)
    
    * Added SupportButton component from PR 74
    
    * Opening Go To Page no longer adjusts the page height, added support button

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
@@ -8,6 +6,8 @@ import Input from '@material-ui/core/Input';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { withStyles } from '@material-ui/core/styles';
 import { Button, Typography, Link } from '@material-ui/core';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 
 import useQuery from '../../hooks/useQuery';
 
@@ -39,6 +39,8 @@ const styles = () => ({
     flexGrow: 1,
     justifyContent: 'center',
     fontSize: '16px',
+    height: '100%',
+
   },
   navigationContainer: {
     display: 'flex',
@@ -90,7 +92,7 @@ const styles = () => ({
   },
   mobilePaginationListContainer: {
     display: 'flex',
-    marginTop: 44,
+    marginTop: 0,
     justifyContent: 'center',
   },
   paginationLabel: {
@@ -108,9 +110,10 @@ const styles = () => ({
     color: '#FFFFFF',
     backgroundColor: '#633AA3',
     borderRadius: '2px',
+    fontWeight: 500,
   },
   hideDisplay: {
-    display: 'none',
+    visibility: 'hidden',
   },
   buttonOpenGoToPage: {
     maxWidth: 28,
@@ -124,6 +127,17 @@ const styles = () => ({
   goToPageLabel: {
     fontSize: 30,
     fontWeight: 700,
+  },
+  centerText: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  alignPaginationRange: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
   },
 });
 
@@ -139,8 +153,8 @@ const NextButton = ({ pageLink, classes }) => {
   const removeUnderline = classes.paginationLabel;
   const nextClasses = `${color} ${removeUnderline} ${classes.expandNavigationButtons}`;
   return (
-    <Link href={pageLink} aria-label="Go next page" className={nextClasses} underline="none">
-      {'>'}
+    <Link href={pageLink} aria-label="Go next page" className={nextClasses} underline="none" data-testid="next-button-icon">
+      <NavigateNextIcon className={classes.centerText} />
     </Link>
   );
 };
@@ -151,9 +165,22 @@ const BackButton = ({ pageLink, classes }) => {
   const backClasses = `${color} ${removeUnderline} ${classes.expandNavigationButtons}`;
   return (
     <Link href={pageLink} aria-label="Go previous page" className={backClasses} underline="none">
-      {'<'}
+      <NavigateBeforeIcon className={classes.centerText} />
     </Link>
   );
+};
+
+const CheckValidPageNumber = (totalPages, input) => {
+  if (typeof input !== 'string' && typeof input !== 'number') {
+    return false;
+  }
+  const pageNum = Number(input);
+  const checkValidType = Number.isInteger(pageNum);
+  const checkValidRange = pageNum >= 1 && pageNum <= totalPages;
+  if (checkValidType && checkValidRange) {
+    return true;
+  }
+  return false;
 };
 
 const GoToPage = ({
@@ -168,13 +195,20 @@ const GoToPage = ({
   const GoToPageClasses = `${classes.goToPageContainer} ${ShowContent}`;
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (CheckValidPageNumber(totalPages, input) === false) {
+      return;
+    }
     navigationObject.history.push(getPageLink(...Object.values(navigationObject), input));
+    navigationObject.history.go(
+      navigationObject.history.location.pathname + navigationObject.history.location.search,
+    );
   };
   return (
     <div className={GoToPageClasses}>
       <form onSubmit={handleSubmit}>
         <span>{label}</span>
         <Input
+          type="number"
           inputProps={{ min: 1, max: totalPages, style: { textAlign: 'center' } }}
           defaultValue={input}
           className={classes.pageInputNavigation}
@@ -185,14 +219,25 @@ const GoToPage = ({
   );
 };
 
-const RangeOfResults = ({ classes, totalCount, calculateRange }) => (
-  (totalCount > 0)
-  && (
-    <Typography variant="h5" align="center" className={classes.showingText}>
-      {calculateRange}
-    </Typography>
-  )
-);
+const RangeOfResults = (
+  {
+    classes, totalCount, calculateRange, desktopDimensions,
+  },
+) => {
+  if (desktopDimensions === false) {
+    return null;
+  }
+  return (
+    (totalCount > 0) && (desktopDimensions)
+    && (
+      <Typography variant="h5" align="center" className={classes.showingText}>
+        <span className={classes.alignPaginationRange}>
+          {calculateRange}
+        </span>
+      </Typography>
+    )
+  );
+};
 
 const OpenGoToPageButton = ({ goToPageLabel, setShowButton, classes }) => {
   const [toggle, setToggle] = useState(true);
@@ -205,7 +250,7 @@ const OpenGoToPageButton = ({ goToPageLabel, setShowButton, classes }) => {
         setShowButton(toggle);
       }}
     >
-      <span classNames={classes.goToPageLabel}>
+      <span className={classes.goToPageLabel}>
         {goToPageLabel}
       </span>
     </Button>
@@ -230,15 +275,23 @@ const PaginationButton = ({
     );
   }
   const isCurrentPage = currPage === pageNumber ? classes.currentPageButton : '';
-  const PaginationButtonClasses = `${classes.paginationLabel} ${isCurrentPage}`;
+  const PaginationButtonClasses = `${classes.paginationLabel} ${classes.activeColor} ${isCurrentPage}`;
+  const PageNumberBox = (
+    <div className={classes.paginationButton}>
+      <span className={classes.centerText}>{pageNumber}</span>
+    </div>
+  );
+  if (currPage === pageNumber) {
+    return (
+      <div className={PaginationButtonClasses} underline="none">
+        {PageNumberBox}
+      </div>
+    );
+  }
   return (
-    <>
-      <Link href={link} aria-label={`Go to page ${pageNumber}`} className={PaginationButtonClasses} underline="none">
-        <div className={classes.paginationButton}>
-          <span>{pageNumber}</span>
-        </div>
-      </Link>
-    </>
+    <Link href={link} aria-label={`Go to page ${pageNumber}`} className={PaginationButtonClasses} underline="none">
+      {PageNumberBox}
+    </Link>
   );
 };
 
@@ -259,6 +312,7 @@ const RenderPaginationButtons = (
       currPage={currPage}
       link={page.link}
       setShowButton={setShowButton}
+      key={page.pageNumber}
     />
   ))
 );
@@ -269,7 +323,7 @@ const calculatePaginationMenu = (totalPages, page, labelForGoToPage, history, qu
     currPage: page,
     nextPage: ((page + 1 < totalPages) ? page + 1 : null),
     nextNextPage: ((page + 2 < totalPages) ? page + 2 : null),
-    ellipsis: ((page + 3 <= totalPages) ? labelForGoToPage : null),
+    ellipsis: ((page + 2 <= totalPages) ? labelForGoToPage : null),
     lastPage: ((totalPages !== page) ? totalPages : null),
   };
   pagesToRender = Object.values(pagesToRender).filter((pgNum) => pgNum !== null);
@@ -277,6 +331,13 @@ const calculatePaginationMenu = (totalPages, page, labelForGoToPage, history, qu
     { pageNumber: pg, link: getPageLink(history, query, perPage, pg) }
   ));
   return pagesWithLinks;
+};
+
+const CalculatePageRange = (page, perPage, totalCount) => {
+  const startingRange = (page - 1) * perPage + 1;
+  const endingRange = (page) * perPage < totalCount ? (page) * perPage : totalCount;
+  const resultString = endingRange <= 1 ? 'Result' : 'Results';
+  return `Showing ${startingRange} - ${endingRange} of ${totalCount} ${resultString}`;
 };
 
 const Pagination = ({
@@ -317,13 +378,6 @@ const Pagination = ({
     nextButton = `${nextLink}?${query.toString()}`;
   }
 
-  const CalculatePageRange = () => {
-    const startingRange = (page - 1) * perPage + 1;
-    const endingRange = (page) * perPage < totalCount ? (page) * perPage : totalCount;
-    const resultString = endingRange <= 1 ? 'Result' : 'Results';
-    return `Showing ${startingRange} - ${endingRange} of ${totalCount} ${resultString}`;
-  };
-
   const navigationObject = {
     history, query, perPage,
   };
@@ -336,7 +390,8 @@ const Pagination = ({
           <RangeOfResults
             totalCount={totalPages}
             classes={classes}
-            calculateRange={CalculatePageRange()}
+            calculateRange={CalculatePageRange(page, perPage, totalCount)}
+            desktopDimensions={useDesktop}
           />
         </div>
         <div className={navigationContainerClasses}>
@@ -380,3 +435,22 @@ Pagination.propTypes = {
 Pagination.defaultProps = {};
 
 export default withStyles(styles)(Pagination);
+
+// eslint-disable-next-line import/no-mutable-exports
+export let Tests = {
+  getPageLink,
+  NextButton,
+  BackButton,
+  CheckValidPageNumber,
+  GoToPage,
+  RangeOfResults,
+  OpenGoToPageButton,
+  PaginationButton,
+  RenderPaginationButtons,
+  calculatePaginationMenu,
+  CalculatePageRange,
+};
+
+if (process.env.NODE_ENV !== 'test') {
+  Tests = undefined;
+}
--- src/components/Pagination/Pagination.test.jsx
+++ src/components/Pagination/Pagination.test.jsx
@@ -1,9 +1,122 @@
+/* eslint-disable  */
 import React from 'react';
-import { render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 
-import Pagination from './Pagination';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+import Pagination, { Tests } from './Pagination';
+import { Check } from '@material-ui/icons';
+
+const {
+  getPageLink,
+  NextButton,
+  BackButton,
+  CheckValidPageNumber,
+  GoToPage,
+  RangeOfResults,
+  OpenGoToPageButton,
+  PaginationButton,
+  RenderPaginationButtons,
+  calculatePaginationMenu,
+  CalculatePageRange,
+} = Tests;
 
 test.skip('Pagination', () => {
   const { asFragment } = render(<Pagination totalCount={50} page={2} perPage={10} />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+describe('Pagination Component', () => {
+  describe('NextButton', () => {
+    const testProps = {
+      classes: {},
+      pageLink: '/',
+    }
+    const { container } = render(<NavigateNextIcon />);
+    const iconHtml = container.innerHTML;
+    cleanup();
+    it('It should have a correct icon label', () => {
+      render(<NextButton {...testProps} />);
+      const nextButton = screen.getByTestId('next-button-icon');
+      expect(nextButton.innerHTML).toBe(iconHtml);
+    });
+    it('It should have an href value if passed in', () => {
+      render(<NextButton {...testProps} />);
+      const nextButton = screen.getByTestId('next-button-icon');
+      expect(nextButton).toHaveAttribute('href', '/');
+    });
+    it('It should not have an href if pageLink was undefined', () => {
+      const noLinkProps = {
+        classes: {},
+        pageLink: undefined,
+      };
+      render(<NextButton {...noLinkProps} />);
+      const nextButton = screen.getByTestId('next-button-icon');
+      expect(nextButton).not.toHaveAttribute('href');
+    });
+    it('It should have a correct aria-label value', () => {
+      render(<NextButton {...testProps} />);
+      expect(screen.getByLabelText('Go next page')).toBeInTheDocument();
+    });
+  });
+
+  describe('CheckValidPageNumber', () => {
+    it('It should return false if the input was not of type string or type number', () => {
+      expect(CheckValidPageNumber(10, null)).toBe(false);
+      expect(CheckValidPageNumber(20, undefined)).toBe(false);
+      expect(CheckValidPageNumber(10, { input: 1 })).toBe(false);
+    });
+    it('It should return false if the value is a string or number but is not a integer', () => {
+      expect(CheckValidPageNumber(10, 4.52)).toBe(false);
+      expect(CheckValidPageNumber(20, '11.11')).toBe(false);
+    });
+    it('It should return false if the input is greater than totalPages', () => {
+      expect(CheckValidPageNumber(1, '2')).toBe(false);
+      expect(CheckValidPageNumber(1000000, 10000001)).toBe(false);
+    });
+    it('It should return true if the input is of type number or string, an integer and within the range 1-totalPages', () => {
+      expect(CheckValidPageNumber(20, '20')).toBe(true);
+      expect(CheckValidPageNumber(20, 20)).toBe(true);
+      expect(CheckValidPageNumber(20, '15')).toBe(true);
+      expect(CheckValidPageNumber(20, 15)).toBe(true);
+      expect(CheckValidPageNumber(40, 1)).toBe(true);
+      expect(CheckValidPageNumber(5000000, 102460)).toBe(true);
+    });
+  });
+
+  describe('RangeOfResults', () => {
+    it('It should not render if it is smaller than the media query, desktopDimensions is false', () => {
+      const noRenderProps = {
+        classes: {},
+        totalCount: 40,
+        calculateRange: 'Showing',
+        desktopDimensions: false,
+      };
+      render(<RangeOfResults {...noRenderProps} />);
+      const rangeOfResults = screen.queryByText('Showing', { exact: false });
+      expect(rangeOfResults).not.toBeInTheDocument();
+    });
+    it('It should not render if the totalCount of results is 0', () => {
+      const noRenderProps = {
+        classes: {},
+        totalCount: 0,
+        calculateRange: 'Showing',
+        desktopDimensions: true,
+      };
+      render(<RangeOfResults {...noRenderProps} />);
+      const rangeOfResults = screen.queryByText('Showing', { exact: false });
+      expect(rangeOfResults).not.toBeInTheDocument();
+    });
+    it('It should render the range of results', () => {
+      const dummyProps = {
+        classes: {},
+        totalCount: 40,
+        calculateRange: `Showing ${5} - ${10} of ${40} results`,
+        desktopDimensions: true,
+      };
+      render(<RangeOfResults {...dummyProps} />);
+      const rangeOfResults = screen.queryByText('Showing', { exact: false });
+      expect(rangeOfResults).toBeInTheDocument();
+    });
+  });
 });
